7854c9415aa049423cd9d1bf8efe7e3a85a7bc91
Felipe Bianchi <fbs96@live.com>
2020-02-13 16:42:34 -0300

    Arquivos até o término da aula Criando model de produto

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const requireDir = require('require-dir'); 
 
 //iniciando o APP
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/nodeapi',{useNewUrlParser:true});
+mongoose.connect
+(
+    'mongodb://localhost:27017/nodeapi',
+    {
+        useNewUrlParser:true
+    }
+);
+requireDir('./src/models');
 
+const Product = mongoose.model('Product');
 
 //Primeira rota
 app.get('/',(req, res)=>
 {
-    res.send('Hello1');
+    Product.create(
+        {
+            title: 'Reactive Native',
+            description: 'Build Native apps with React ',
+            url: 'http://github.com/facebook/react-native',
+        });
+    return res.send('Hello1');
 });
 app.listen(3001);
