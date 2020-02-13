6ede7d37b9b37199dd7455d6f108fc0a75370801
Felipe Bianchi <fbs96@live.com>
2020-02-13 16:23:32 -0300

    Arquivos até o término da aula Conectando Database

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+
+//iniciando o APP
 const app = express();
 
+mongoose.connect('mongodb://localhost:27017/nodeapi',{useNewUrlParser:true});
+
+
+//Primeira rota
 app.get('/',(req, res)=>
 {
     res.send('Hello1');
