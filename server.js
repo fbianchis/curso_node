1b74837b9efa8e140bbc5afbb8cf8fe0a718d9c1
Felipe Bianchi <fbs96@live.com>
2020-02-14 11:08:17 -0300

    Arquivos até o término da aula Criação de registro

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const requireDir = require('require-dir');
 
 //iniciando o APP
 const app = express();
-
+app.use(express.json());
 mongoose.connect
 (
     'mongodb://localhost:27017/nodeapi',
--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -9,4 +9,11 @@ module.exports =
         const products = await Product.find();
         return res.json(products);
     },
+
+    async store(req, res)
+    {
+        //criação
+        const product = await Product.create(req.body);
+        return res.json(products);
+    }
 };
